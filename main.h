fce1d603f67423acc17d6febf0b2e1010038c789
hdh3296 <hdh3296@gmail.com>
2018-02-22 14:35:30 +0900

    1채널에 대하여 모든 기능 테스트 완료

---
+++ main.h
@@ -0,0 +1,32 @@
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define __TEMP_1000		1
+
+#define	TRUE	1
+#define	FALSE	0
+
+
+#define ON_LED		0
+#define OFF_LED		1
+
+#define	ON_RELAY	1
+#define OFF_RELAY	0
+
+
+typedef enum
+{
+	T21 = 2668,	T22 = 2698,	T23 = 2729,	T24 = 2759, T25 = 2790,
+	T26 = 2820, T27 = 2850,	T28 = 2880,	T29 = 2910,	T30 = 2940,
+	T31 = 2970, T32 = 3000,	T33 = 3029,	T34 = 3059,	T35 = 3089,
+	T36 = 3118, T37 = 3148,	T38 = 3177,	T39 = 3206,	T40 = 3235,
+	T41 = 3264, T42 = 3293, T43 = 3322, T44 = 3351, T45 = 3380,
+	T46 = 3409, T47 = 3437, T48 = 3466, T49 = 3494, T50 = 3523
+} tag_TempValueDef;
+
+
+
+
+#endif
+
